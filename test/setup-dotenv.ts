9ef9bda979b041d794a346d4a2ce62bda126778d
Wan <495709+wa0x6e@users.noreply.github.com>
2026-09-26 22:43:38 +0400

    chore(deps): upgrade dotenv to v18 (#690)

--- test/setup-dotenv.ts
+++ test/setup-dotenv.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import dotenv from 'dotenv';
 
 // Load .env file from root
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
+dotenv.config({ path: path.resolve(__dirname, '../.env'), quiet: true });
 
 // Load .env.test file from test directory (overwrites any duplicate keys)
-dotenv.config({ path: path.resolve(__dirname, '.env.test'), override: true });
+dotenv.config({ path: path.resolve(__dirname, '.env.test'), override: true, quiet: true });
